4ed0a629b836604fb9cab8355a871f54037ff156
Leon Warren <leon.warren@stud.h-da.de>
2023-07-27 12:51:42 +0200

    init

---
+++ Client/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+
+
+
+
+int main(){
+
+
+    std::cout << "Chat App" << std::endl;
+    std::cout << "-------------------" << std::endl;
+    std::cout << "1. Login" << std::endl;
+    std::cout << "2. Register" << std::endl;
+    std::cout << "3. Exit" << std::endl;
+    std::cout << "-------------------" << std::endl;
+    
+
+
+
+}
